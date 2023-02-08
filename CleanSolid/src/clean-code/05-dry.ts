e18a3f79dff8a043e05e9cf50a3791ec87e5bb15
Maximiliano Cóppola <maxicopp@outlook.com>
2023-02-08 05:08:15 -0300

    Aplicando Dry

--- CleanSolid/src/clean-code/05-dry.ts
+++ CleanSolid/src/clean-code/05-dry.ts
@@ -7,16 +7,31 @@ class Product {
     public size: Size = ''
   ) {}
 
+  isProductReady(): boolean {
+    for (const key in this) {
+      switch (typeof this[key]) {
+        case 'string':
+          if ((this[key] as string).length <= 0)
+            throw new Error(`${key} is empty`);
+          break;
+        case 'number':
+          if (this[key] <= 0) throw new Error(`${key} is zero`);
+          break;
+        default:
+          throw new Error(`${typeof this[key]} is not supported`);
+      }
+    }
+    return true;
+  }
+
   toString() {
-    if (this.name.length <= 0) throw new Error('name is empty');
-    if (this.price <= 0) throw new Error('price is zero');
-    if (this.size.length <= 0) throw new Error('size is empty');
+    if (!this.isProductReady) return;
 
     return `${this.name} (${this.price}), ${this.size}`;
   }
 }
 
 (() => {
-  const bluePants = new Product('Blue Large Pants', 10, 'M');
+  const bluePants = new Product('Blue Large Pants', 10, 'S');
   console.log(bluePants.toString());
 })();
