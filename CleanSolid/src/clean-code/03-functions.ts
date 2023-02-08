c3e8942e5e8efb55300f383a9aa078009807688c
Maximiliano Cóppola <maxicopp@outlook.com>
2023-02-08 02:59:55 -0300

    Detalles adicionales sobre funciones

--- CleanSolid/src/clean-code/03-functions.ts
+++ CleanSolid/src/clean-code/03-functions.ts
@@ -28,4 +28,17 @@
     console.log('Crear actor', birthdate);
     return true;
   }
+
+  // Continuar
+  const getPayAmount = ({
+    isDead = false,
+    isSeparated = true,
+    isRetired = false,
+  }): number => {
+    if (isDead) return 1500;
+
+    if (isSeparated) return 2500;
+
+    return isRetired ? 3000 : 4000;
+  };
 })();
