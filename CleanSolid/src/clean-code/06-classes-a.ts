73cf1226e2d55619f7b1cedcca2f2e0f666bec64
Maximiliano Cóppola <maxicopp@outlook.com>
2023-02-09 02:10:43 -0300

    Herencia - Problemática

--- CleanSolid/src/clean-code/06-classes-a.ts
+++ CleanSolid/src/clean-code/06-classes-a.ts
@@ -1,4 +1,6 @@
 (() => {
+  // No aplicando el principio de responsabilidad única
+
   type Gender = 'M' | 'F';
 
   class Person {
@@ -9,6 +11,47 @@
     ) {}
   }
 
-  const newPerson = new Person('Max', 'M', new Date('1995-06-12'));
-  console.log({ newPerson });
+  class User extends Person {
+    public lastAccess: Date;
+    constructor(
+      public email: string,
+      public role: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ) {
+      super(name, gender, birthdate);
+      this.lastAccess = new Date();
+    }
+
+    checkCredentials() {
+      return true;
+    }
+  }
+
+  class UserSettings extends User {
+    constructor(
+      public workingDirectory: string,
+      public lastOpenFolder: string,
+      email: string,
+      role: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ) {
+      super(email, role, name, gender, birthdate);
+    }
+  }
+
+  const userSettings = new UserSettings(
+    '/usr/home',
+    '/home',
+    'maxicopp@google.com',
+    'Admin',
+    'Maximiliano',
+    'M',
+    new Date('1995-06-12')
+  );
+
+  console.log({ userSettings });
 })();
