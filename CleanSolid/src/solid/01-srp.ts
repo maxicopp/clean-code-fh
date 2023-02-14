fe823afa8f3df1bdaad6f5dd3caf6d5fc8bfba3d
Maximiliano Cóppola <maxicopp@outlook.com>
2023-02-14 02:17:10 -0300

    Ejemplo de SRP

--- CleanSolid/src/solid/01-srp.ts
+++ CleanSolid/src/solid/01-srp.ts
@@ -22,21 +22,25 @@
     }
   }
 
-  // Usualmente, esto es una clase para controlar la vista que es desplegada al usuario
-  // Recuerden que podemos tener muchas vistas que realicen este mismo trabajo.
   class ProductBloc {
+    private productService: ProductService;
+    private mailer: Mailer;
+
+    constructor(productService: ProductService, mailer: Mailer) {
+      this.productService = productService;
+      this.mailer = mailer;
+    }
+
     loadProduct(id: number) {
-      // Realiza un proceso para obtener el producto y retornarlo
-      console.log('Producto: ', { id, name: 'OLED Tv' });
+      this.productService.getProduct(id);
     }
 
     saveProduct(product: Product) {
-      // Realiza una petición para salvar en base de datos
-      console.log('Guardando en base de datos', product);
+      this.productService.saveProduct(product);
     }
 
     notifyClients() {
-      console.log('Enviando correo a los clientes');
+      this.mailer.sendEmail(['max@google.com'], 'to-clients');
     }
   }
 
@@ -48,7 +52,9 @@
     }
   }
 
-  const productBloc = new ProductBloc();
+  const productService = new ProductService();
+  const mailer = new Mailer();
+  const productBloc = new ProductBloc(productService, mailer);
   const cartBloc = new CartBloc();
 
   productBloc.loadProduct(10);
