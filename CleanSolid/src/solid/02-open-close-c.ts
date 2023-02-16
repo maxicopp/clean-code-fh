08f3b015c4cb7794757114c51df0e3ae735bae22
Maximiliano Cóppola <maxicopp@outlook.com>
2023-02-16 05:36:23 -0300

    Remover la dependencia de axios

--- CleanSolid/src/solid/02-open-close-c.ts
+++ CleanSolid/src/solid/02-open-close-c.ts
@@ -1,8 +1,20 @@
-import axios from 'axios';
+export interface HttpResponse<T> {
+  data: T;
+  status: number;
+}
 
 export class HttpClient {
-  async get(url: string) {
-    const { data, status } = await axios.get(url);
-    return { data, status };
+  async get<T>(
+    url: string,
+    headers?: Record<string, string>
+  ): Promise<HttpResponse<T>> {
+    try {
+      const response = await fetch(url, { headers });
+      const data = (await response.json()) as T;
+      return { data, status: response.status };
+    } catch (error) {
+      console.error(`Error occurred while fetching ${url}: ${error}`);
+      throw error;
+    }
   }
 }
