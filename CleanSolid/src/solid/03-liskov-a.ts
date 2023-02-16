2201bd0bdf661a4e980b8718d8047f70f67727dd
Maximiliano Cóppola <maxicopp@outlook.com>
2023-02-16 06:25:08 -0300

    Solución aplicando los principios OSP y LSP

--- CleanSolid/src/solid/03-liskov-a.ts
+++ CleanSolid/src/solid/03-liskov-a.ts
@@ -1,29 +1,18 @@
-import { Tesla, Audi, Toyota, Honda, Volvo } from './03-liskov-b';
+import {
+  Audi,
+  Ford,
+  Honda,
+  Tesla,
+  Toyota,
+  Vehicle,
+  Volvo,
+} from './03-liskov-b';
 
 (() => {
-  const printCarSeats = (cars: (Tesla | Audi | Toyota | Honda | Volvo)[]) => {
-    for (const car of cars) {
-      if (car instanceof Tesla) {
-        console.log('Tesla', car.getNumberOfTeslaSeats());
-        continue;
-      }
-      if (car instanceof Audi) {
-        console.log('Audi', car.getNumberOfAudiSeats());
-        continue;
-      }
-      if (car instanceof Toyota) {
-        console.log('Toyota', car.getNumberOfToyotaSeats());
-        continue;
-      }
-      if (car instanceof Honda) {
-        console.log('Honda', car.getNumberOfHondaSeats());
-        continue;
-      }
-      if (car instanceof Volvo) {
-        console.log('Volvo', car.getNumberOfVolvoSeats());
-        continue;
-      }
-    }
+  const printCarSeats = (cars: Vehicle[]) => {
+    cars.forEach((car) => {
+      console.log(car.constructor.name, car.getNumberOfSeats());
+    });
   };
 
   const cars = [
@@ -32,6 +21,7 @@ import { Tesla, Audi, Toyota, Honda, Volvo } from './03-liskov-b';
     new Toyota(5),
     new Honda(5),
     new Volvo(2),
+    new Ford(2),
   ];
 
   printCarSeats(cars);
